85db6218a43b22c06c8e04b81d99acd3ba0aa9a1
mmDesign25 <maedam2025@gmail.com>
2025-11-24 14:25:02 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const header = document.querySelector('.header');
+const logo = document.getElementById('logo');
+
+// スクロール時の処理
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+        // ロゴをダークに切り替え
+
+        // 学校のサーバ用
+        // logo.src = '/2865/sunday-flower/icons/logo-dark.svg';
+
+        // MAMP用
+        // logo.src = 'icons/logo-dark.svg';
+
+        // ブラウザ用(indexを右クリック＆Open with Live Server)
+        logo.src = '/icons/logo-dark.svg';
+
+
+    } else {
+        header.classList.remove('scrolled');
+        // ロゴをライトに戻す
+
+        // 学校のサーバ用
+        // logo.src = '/2865/sunday-flower/icons/logo-light.svg';
+
+        // MAMP用
+        // logo.src = 'icons/logo-light.svg';
+
+        // ブラウザ用(indexを右クリック＆Open with Live Server)
+        logo.src = '/icons/logo-light.svg';
+    }
+});
